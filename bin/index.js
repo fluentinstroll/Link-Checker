75dd38c180e179b862063a8ac0019f64de2a1476
Joy3van <44476753+Joy3van@users.noreply.github.com>
2020-10-16 22:42:23 -0400

    Resolve issue7

--- bin/index.js
+++ bin/index.js
@@ -4,6 +4,15 @@ const chalk = require("chalk");
 const yargs = require("yargs");
 const fs = require("fs");
 const {
+    once
+} = require('events');
+const {
+    createReadStream
+} = require('fs');
+const {
+    createInterface
+} = require('readline');
+const {
     argv,
     exit
 } = require("process");
@@ -13,6 +22,8 @@ require('events').EventEmitter.defaultMaxListeners = 11;
 
 req.maxRedirects = 11;
 
+var iFileArr = []; // an array for storing each line of the ignore file -Joy3van
+
 const options = yargs
     .usage("Usage $0: enter filename after command, with a proper argument.")
     .demandCommand(1)
@@ -21,22 +32,50 @@ const options = yargs
     .describe('g', 'Show only good links in a file.')
     .alias('b', 'bad')
     .describe('b', 'Show only bad links in a file')
+    .alias('i', 'ignore')
+    .describe('i', 'Ignore links provided in another file, the path of ignore file should be after argument')
     .argv;
 
+// Process the ignore file -Joy3van
+if (options.i) {
+    (async function processLineByLine() {
+        try {
+            const rl = createInterface({
+                input: createReadStream(`${argv[4]}`),
+                crlfDelay: Infinity
+            });
 
+            rl.on('line', (line) => {
+                if (line[0] !== '#') {
+                    if (line[0] === 'h') {
+                        iFileArr.push(line);
+                    } else {
+                        rl.close();
+                    }
+                }
+            });
+
+            await once(rl, 'close');
+
+        } catch (err) {
+            console.log('Please check if you typed the options in a correct order');
+        }
+    })();
+
+}
 
 fs.readFile(`${argv[2]}`, (err, data) => {
     try {
-    let linklist = generateLinkList(data);
-    linklist = Array.from(linklist);
-}
-catch (err) {
-    console.log("The app has recieved a wrong filename.")
-    console.log("Please enter a correct filename.")
-    exit(1);
-}
+        var linklist = generateLinkList(data); // Changed let to var because otherwise linklist variable would become undefined outside the try block -Joy3van
+        linklist = Array.from(linklist);
+    } catch (err) {
+        console.log("The app has recieved a wrong filename.")
+        console.log("Please enter a correct filename.")
+        exit(1);
+    }
     validateLinks(linklist)
 })
+
 
 
 const generateLinkList = (data) => {
@@ -45,7 +84,17 @@ const generateLinkList = (data) => {
 }
 
 const separateLinks = (data) => {
-    let list = getUrls(data.toString());
+    let list;
+    if (options.i) {
+        list = getUrls(data.toString(), {
+            stripWWW: false,
+            exclude: iFileArr
+        }); // Set exclude option and exclude whatever link had been put in iFileArr. - Joy3van
+    } else {
+        list = getUrls(data.toString(), {
+            stripWWW: false
+        }); // Set the stripWWW option to false so it would get correct links(default is true) -Joy3van
+    }
     return list;
 }
 
@@ -75,7 +124,7 @@ const isValid = (link) => {
             if (status === 200) {
                 if (!options.b) {
                     console.log(chalk.green(`[200] GOOD ${link}`));
-                    if (process.exitCode != 1 && process.exitCode != 2){
+                    if (process.exitCode != 1 && process.exitCode != 2) {
                         process.exitCode = 0;
                     }
                 }
